984871f07c67dce8f78ad99730f98b4b7053acef
yaoyang <kiki@smarty.so>
2020-01-16 13:22:01 +0800

    Fork from git@github.com:smarty-kiki/api_frame.git and modify project name

--- project/tool/production/after_push.sh
+++ project/tool/production/after_push.sh
@@ -2,15 +2,15 @@
 
 ROOT_DIR="$(cd "$(dirname $0)" && pwd)"/../../..
 
-ln -fs $ROOT_DIR/project/config/production/nginx/api_frame.conf /etc/nginx/sites-enabled/api_frame
+ln -fs $ROOT_DIR/project/config/production/nginx/customer_api.conf /etc/nginx/sites-enabled/customer_api
 /usr/sbin/service nginx reload
 
 /bin/bash $ROOT_DIR/project/tool/dep_build.sh link
 /usr/bin/php $ROOT_DIR/public/cli.php migrate:install
 /usr/bin/php $ROOT_DIR/public/cli.php migrate
 
-ln -fs $ROOT_DIR/project/config/production/supervisor/api_frame_queue_worker.conf /etc/supervisor/conf.d/api_frame_queue_worker.conf
+ln -fs $ROOT_DIR/project/config/production/supervisor/customer_api_queue_worker.conf /etc/supervisor/conf.d/customer_api_queue_worker.conf
 /usr/bin/supervisorctl update
-/usr/bin/supervisorctl restart api_frame_queue_worker:*
+/usr/bin/supervisorctl restart customer_api_queue_worker:*
 
-ln -fs $ROOT_DIR/project/config/production/crontab/api_frame /etc/cron.d/api_frame
+ln -fs $ROOT_DIR/project/config/production/crontab/customer_api /etc/cron.d/customer_api
--- project/tool/start_development_server.sh
+++ project/tool/start_development_server.sh
@@ -4,11 +4,11 @@ ROOT_DIR="$(cd "$(dirname $0)" && pwd)"/../..
 
 sh $ROOT_DIR/project/tool/dep_build.sh link
 
-sudo docker run --rm -ti -p 80:80 -p 8080:8080 -p 3306:3306 --name api_frame \
+sudo docker run --rm -ti -p 80:80 -p 8080:8080 -p 3306:3306 --name customer_api \
     -v $ROOT_DIR/../frame:/var/www/frame \
-    -v $ROOT_DIR/:/var/www/api_frame \
-    -v $ROOT_DIR/project/config/development/nginx/api_frame.conf:/etc/nginx/sites-enabled/default \
-    -v $ROOT_DIR/project/config/development/supervisor/api_frame_queue_worker.conf:/etc/supervisor/conf.d/api_frame_queue_worker.conf \
+    -v $ROOT_DIR/:/var/www/customer_api \
+    -v $ROOT_DIR/project/config/development/nginx/customer_api.conf:/etc/nginx/sites-enabled/default \
+    -v $ROOT_DIR/project/config/development/supervisor/customer_api_queue_worker.conf:/etc/supervisor/conf.d/customer_api_queue_worker.conf \
     -e 'TIMEZONE=Asia/Shanghai' \
-    -e 'AFTER_START_SHELL=/var/www/api_frame/project/tool/development/after_env_start.sh' \
+    -e 'AFTER_START_SHELL=/var/www/customer_api/project/tool/development/after_env_start.sh' \
 kikiyao/debian_php_dev_env start
